9adbd185d03bdfd605ccdf3a201140acbb84a4fc
Yagel Sheli <yagelsheli@gmail.com>
2015-01-22 00:35:26 +0200

    BigBang commit part 1 Added new classes: 1. Connection - for easy managing of clients. 2. Package - An object represents package. 3. PackageManager - for managing the packages.
    Also added support and communication between Packages db and Users db.
    
    Multi-user support added as well and everything is working perfectly.

--- Connection.py
+++ Connection.py
@@ -1,5 +1,25 @@
 __author__ = 'yagel'
 
-class Connection:
-    pass
 
+import time
+
+
+class Connection:
+    def __init__(self, sock, details):
+        self.sock = sock
+        self.details = details
+        self.last_act = time.time()
+
+    def send(self, message):
+        self.last_act = time.time()
+        self.sock.send(message)
+
+    def recv(self, len):
+        self.last_act = time.time()
+        return self.sock.recv(len)
+
+    def isActive(self):
+        return not (time.time() - self.last_act > 120)
+
+    def close(self):
+        self.sock.close()
