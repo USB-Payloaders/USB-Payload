efa3b70e689c6785a5d5b66c436315d08d44fe12
omerporze <omer.porze@gmail.com>
2015-05-05 17:58:00 +0300

    Some changes to server and client

---
+++ clientHeader.h
@@ -0,0 +1,23 @@
+#ifndef clientHeader
+#define clientHeader
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string>
+#include <iostream>
+#include <WinSock2.h>
+#include <Windows.h>
+#include <ws2tcpip.h>
+#include <sstream>
+#include <stdio.h>
+#include "Package.h"
+#include "PackageQue.h"
+#include <vector>
+#include <iterator>
+#include <queue>
+#include <stack>
+
+
+
+#endif
